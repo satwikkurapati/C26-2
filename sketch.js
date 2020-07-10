4e416324eb64f251258dda6b0548de4a64f8430e
satwikkurapati <satwikkurapati1@gmail.com>
2020-07-10 19:25:06 -0400

    add platform

--- sketch.js
+++ sketch.js
@@ -15,9 +15,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    ground = new Ground(600,height,1200,20);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -58,7 +57,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
-    bird.display();
     platform.display();
+    bird.display();
+    
 }
